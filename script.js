ab28d5c552a6b5142cc6d0f0942596db86f4d539
Karlo Domjanić <karlo.domjanic@sigit.hr>
2023-06-30 16:15:38 +0200

    .

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-// Subscribe to Newsletter
-function subscribeToNewsletter() {
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const emailInput = document.getElementById('emailInput');
-    const emailWarning = document.getElementById('email-warning')
+// const modal = document.querySelector('#modal')
+const main = document.getElementById('main')
+const modal = document.getElementById('modal')
 
-    if (!emailInput) {
-        alert('Please provide email')
-    } else if (!emailPattern.test(emailInput)) {
+const emailInput = document.getElementById('emailInput');
+const subscribeButton = document.getElementById('subscribeToNewsletter')
+const dismissButton = document.getElementById('dismissButton')
+const emailWarning = document.getElementById('email-warning')
+
+const userMail = document.getElementById('userMail')
+
+// Subscribe to newsletter
+subscribeButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const email = emailInput.value.trim()
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailPattern.test(email) || !email) {
         emailInput.classList.add('invalid');
         emailWarning.removeAttribute('hidden')
-    }   else {
+    } else {
         emailInput.classList.remove('invalid');
         emailWarning.setAttribute('hidden', '')
+        userMail.textContent = emailInput.value.trim()
+        main.classList.add('hidden')
+        modal.classList.remove('hidden')
     }
-}
+})
+
+// Dismiss message in modal
+dismissButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    main.classList.remove('hidden');
+    modal.classList.add('hidden');
+    emailInput.value = ''
+})
